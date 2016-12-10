5750c756753971b3ed628d7a17cb449431333837
Alex Ford <alexford@frontside.io>
2016-12-10 12:56:38 -0600

    Make a Request Locally

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
+import ENV from "austin-speaks-too/config/environment";
 
 export default Ember.Route.extend({
-
-  model: function(params) {
-
+  model: function() {
+    return $.ajax({
+      url: 'https://api.meetup.com/find/groups',
+      data:{
+        category: 34,
+        page: 30,
+        order: 'members',
+        sign: true,
+        key: ENV.MEETUP_KEY
+      },
+      dataType: 'jsonp'
+    });
   }
 });
