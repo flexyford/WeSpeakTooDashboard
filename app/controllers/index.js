e45ee161d25c369208758d5ba2648a8c4bd05091
Alex Ford <alexford@frontside.io>
2016-12-18 12:23:22 -0600

    Import From CSV
    Still need to make REAL requests to fill-in missing information

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,62 @@
 import Ember from 'ember';
+import PapaParse from 'papaparse';
+
 const { get, set } = Ember;
 
 export default Ember.Controller.extend({
   actions: {
+    import(files) {
+      let csv = files[0];
+      debugger;
+      let data = PapaParse.parse(csv, {
+        complete: (results) => {
+          let { data } = results;
+          let columns = data.shift();
+
+          let indeces = {
+            group: columns.indexOf('group'),
+            event: columns.indexOf('event'),
+            male: columns.indexOf('male'),
+            female: columns.indexOf('female'),
+            nonBinary: columns.indexOf('non-binary')
+          };
+
+          let events = data.map((result) => {
+            return {
+              id: result[indeces.event],
+              male_speakers: result[indeces.male],
+              female_speakers: result[indeces.female],
+              non_binary_speakers: result[indeces.nonBinary],
+              group_id: result[indeces.group]
+            };
+          });
+
+          let groups = data.reduce((groups, result) => {
+            let id = result[indeces.group];
+            if (!groups.find((g) => g.id === id)) {
+              return groups.concat({
+                id
+              });
+            }
+            return groups;
+          }, []);
+
+          let payload = {
+            groups,
+            events
+          };
+
+          debugger;
+
+          get(this, 'store').pushPayload(payload);
+
+          // this.transitionToRoute('groups.group.events', {
+          //   selected: groups.mapBy('id'),
+          //   events:
+          // });
+        }
+      });
+    },
     submit(queryParams) {
       this.transitionToRoute('groups', { queryParams });
     }
--- app/serializers/event.js
+++ app/serializers/event.js
@@ -3,19 +3,22 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   normalize(model, hash, prop) {
-    // `hash.id` is not always guaranteed to be unqiue but the
-    // `event_url` identifier is unqiue so we strip that instead
-    const re_find_index = new RegExp(/events\/(\d+)\//);
-    let matches = hash.event_url.match(re_find_index);
-    if (matches) { hash.id = matches.pop(); }
+    if(hash['event_url']) {
+      // `hash.id` is not always guaranteed to be unqiue but the
+      // `event_url` identifier is unqiue so we strip that instead
+      const re_find_index = new RegExp(/events\/(\d+)\//);
+      let matches = hash.event_url.match(re_find_index);
+      if (matches) { hash.id = matches.pop(); }
+    }
+
     hash.rsvp = hash['yes_rsvp_count'];
 
     hash.description = Ember.String.htmlSafe(hash.description);
 
     // Default to zero Speakers when fetching from Meetup API
-    hash.maleSpeakers = 0;
-    hash.femaleSpeakers = 0;
-    hash.nonBinarySpeakers = 0;
+    hash.maleSpeakers = hash['male_speakers'] || 0;
+    hash.femaleSpeakers = hash['female_speakers'] || 0;
+    hash.nonBinarySpeakers = hash['non_binary_speakers'] || 0;
     return this._super(...arguments);
   }
 });
