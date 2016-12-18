479598229fdde70c4a68b018097f298418370065
Alex Ford <alexford@frontside.io>
2016-12-17 22:03:24 -0600

    Guarantee Unique `event` id

--- app/serializers/event.js
+++ app/serializers/event.js
@@ -3,7 +3,13 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   normalize(model, hash, prop) {
+    // `hash.id` is not always guaranteed to be unqiue but the
+    // `event_url` identifier is unqiue so we strip that instead
+    const re_find_index = new RegExp(/events\/(\d+)\//);
+    let matches = hash.event_url.match(re_find_index);
+    if (matches) { hash.id = matches.pop(); }
     hash.rsvp = hash['yes_rsvp_count'];
+
     hash.description = Ember.String.htmlSafe(hash.description);
 
     // Default to zero Speakers when fetching from Meetup API
