58d7a7b682f41308aff63230cb3ae1b3ee2fa8f0
Alex Ford <alexford@frontside.io>
2016-12-17 21:27:17 -0600

    Hardcode all meetup categories

--- app/components/meetup-group-form.js
+++ app/components/meetup-group-form.js
@@ -3,33 +3,28 @@ import ENV from "austin-speaks-too/config/environment";
 
 const { get, set } = Ember;
 
+import { CATEGORIES } from 'austin-speaks-too/utils/meetup';
+
 export default Ember.Component.extend({
   params: null,
 
   'on-success': Ember.K,
   'on-error': Ember.K,
 
-  endpoint: 'groups',
+  categories: CATEGORIES,
 
-  // default query params
-  page: 20,
-  category: 34, // tech
-  order: 'members',
-
-  init: function() {
-    this._super(...arguments);
-    let params = Object.assign({}, {
-      category: get(this, 'category'),
-      order: get(this, 'order'),
-      page: get(this, 'page')
-    });
-
-    set(this, 'params', params);
-  },
+  defaults: Ember.computed('categories', function() {
+    return {
+      category: get(this, 'categories').findBy('name', 'Tech').value,
+      order: 'members',
+      page: 20
+    };
+  }),
 
   actions: {
     submit(changeset) {
-      let params = Object.assign({}, get(this, 'params'), changeset);
+      debugger;
+      let params = Object.assign({}, get(this, 'defaults'), changeset);
       get(this, 'on-submit')(params);
     }
   }
